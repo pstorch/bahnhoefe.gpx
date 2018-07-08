dd4aaf508117d284fa80f986dc370fa894b3a5b1
Peter Storch <peter.storch@gmail.com>
2018-07-08 22:23:30 +0200

    add final modifier

--- src/main/java/org/railwaystations/api/monitoring/Monitor.java
+++ src/main/java/org/railwaystations/api/monitoring/Monitor.java
@@ -3,5 +3,5 @@ package org.railwaystations.api.monitoring;
 public interface Monitor {
     void sendMessage(final String message);
 
-    void sendMessage(String responseUrl, String message);
+    void sendMessage(final String responseUrl, final String message);
 }
