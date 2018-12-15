8e48ad176a0f94cf248440445aa81551fe83edff
Peter Storch <peter.storch@gmail.com>
2018-12-15 22:45:09 +0100

    consume body to prevent response to be swallowed
    401 response on photo upload will be swollowed by Apache because it can't write the body to rsapi.

--- src/main/java/org/railwaystations/api/auth/UploadTokenAuthFilter.java
+++ src/main/java/org/railwaystations/api/auth/UploadTokenAuthFilter.java
@@ -1,11 +1,15 @@
 package org.railwaystations.api.auth;
 
 import io.dropwizard.auth.AuthFilter;
+import org.apache.commons.io.output.NullOutputStream;
+import org.apache.http.entity.InputStreamEntity;
 import org.eclipse.jetty.http.HttpStatus;
 
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.core.SecurityContext;
+import java.io.IOException;
+import java.io.InputStream;
 import java.security.Principal;
 
 public class UploadTokenAuthFilter<P extends Principal> extends AuthFilter<UploadTokenCredentials, P> {
@@ -17,9 +21,21 @@ public class UploadTokenAuthFilter<P extends Principal> extends AuthFilter<Uploa
                         requestContext.getHeaders().getFirst("Email"),
                         requestContext.getHeaders().getFirst("Upload-Token"));
         if (!authenticate(requestContext, credentials, SecurityContext.BASIC_AUTH)) {
+            consumeBody(requestContext.getEntityStream());
             throw new WebApplicationException(HttpStatus.UNAUTHORIZED_401);
         }
     }
+
+    private void consumeBody(final InputStream body) {
+        if (body != null) {
+            final InputStreamEntity inputStreamEntity = new InputStreamEntity(body);
+            try {
+                inputStreamEntity.writeTo(new NullOutputStream());
+            } catch (final IOException ignored) {
+            }
+        }
+    }
+
 
     public static class Builder<P extends Principal>
             extends AuthFilterBuilder<UploadTokenCredentials, P, UploadTokenAuthFilter<P>> {
