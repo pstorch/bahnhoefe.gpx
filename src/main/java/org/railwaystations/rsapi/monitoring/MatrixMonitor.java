186c5c04405b3bf9299660018ba13a6f06b34263
Peter Storch <peter.storch@gmail.com>
2021-06-21 13:17:34 +0200

    migrated MatrixMonitor

--- src/main/java/org/railwaystations/rsapi/monitoring/MatrixMonitor.java
+++ src/main/java/org/railwaystations/rsapi/monitoring/MatrixMonitor.java
@@ -1,43 +1,39 @@
 package org.railwaystations.rsapi.monitoring;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.apache.http.client.config.RequestConfig;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.ByteArrayEntity;
-import org.apache.http.entity.ContentType;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.util.EntityUtils;
 import org.railwaystations.rsapi.utils.ImageUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.concurrent.Executors;
 
+@Service
 public class MatrixMonitor implements Monitor {
 
     private static final ObjectMapper MAPPER = new ObjectMapper();
     private static final Logger LOG = LoggerFactory.getLogger(MatrixMonitor.class);
 
-    private final CloseableHttpClient httpclient;
+    private final WebClient webClient;
+
+    @Value("${matrix.roomUrl}")
     private String roomUrl;
+    @Value("${matrix.uploadUrl}")
     private String uploadUrl;
+    @Value("${matrix.accessToken}")
     private String accessToken;
 
 
-    public MatrixMonitor() {
+    public MatrixMonitor(final WebClient.Builder webClientBuilder) {
         super();
-        this.httpclient = HttpClients.custom().setDefaultRequestConfig(
-                RequestConfig.custom()
-                        .setSocketTimeout(5000)
-                        .setConnectTimeout(5000)
-                        .setConnectionRequestTimeout(10000).build()
-        ).build();
+        this.webClient = webClientBuilder.clone().build();
     }
 
     @Override
@@ -52,53 +48,40 @@ public class MatrixMonitor implements Monitor {
     protected void sendMessageInternal(final String message, final File photo) {
         LOG.info("Sending message: {}", message);
         try {
-            final CloseableHttpResponse response = sendRoomMessage(new MatrixTextMessage(message));
-            final int status = response.getStatusLine().getStatusCode();
-            final String content = EntityUtils.toString(response.getEntity());
-            if (status >= 200 && status < 300) {
-                LOG.info("Got json response: {}", content);
-            } else {
-                LOG.error("Error reading json, status {}: {}", status, content);
-            }
+            final JsonNode response = sendRoomMessage(new MatrixTextMessage(message));
+            LOG.info("Got json response: {}", response.toString());
 
             if (photo != null) {
                 sendPhoto(photo);
             }
+
         } catch (final RuntimeException | IOException e) {
             LOG.warn("Error sending MatrixMonitor message", e);
         }
     }
 
-    private CloseableHttpResponse sendRoomMessage(final Object message) throws IOException {
-        final String json = MAPPER.writeValueAsString(message);
-        final HttpPost httpPost = new HttpPost(roomUrl + "?access_token=" + accessToken);
-        httpPost.setEntity(new StringEntity(json, ContentType.APPLICATION_JSON.withCharset("UTF-8")));
-        return httpclient.execute(httpPost);
+    private JsonNode sendRoomMessage(final Object message) throws IOException {
+        return this.webClient.post()
+                .uri(roomUrl + "?access_token=" + accessToken)
+                .contentType(MediaType.APPLICATION_JSON_UTF8)
+                .bodyValue(message)
+                .retrieve()
+                .bodyToMono(JsonNode.class)
+                .block();
     }
 
     private void sendPhoto(final File photo) throws IOException {
-        final HttpPost httpPost = new HttpPost(uploadUrl + "?filename" + photo.getName() + "&access_token=" + accessToken);
-        httpPost.setEntity(new ByteArrayEntity(ImageUtil.scalePhoto(photo, 300), ContentType.getByMimeType(ImageUtil.extensionToMimeType(ImageUtil.getExtension(photo.getName())))));
-        final CloseableHttpResponse responseUpload = httpclient.execute(httpPost);
-        final int statusUpload = responseUpload.getStatusLine().getStatusCode();
-        final String contentUpload = EntityUtils.toString(responseUpload.getEntity());
-        if (statusUpload >= 200 && statusUpload < 300) {
-            LOG.info("Got json response: {}", contentUpload);
-        } else {
-            LOG.error("Error reading json, statusUpload {}: {}", statusUpload, contentUpload);
-            return;
-        }
+        final MatrixUploadResponse matrixUploadResponse = this.webClient.post()
+                .uri(uploadUrl + "?filename" + photo.getName() + "&access_token=" + accessToken)
+                .contentType(ContentType.getByMimeType(ImageUtil.extensionToMimeType(ImageUtil.getExtension(photo.getName())))
+                .bodyValue(ImageUtil.scalePhoto(photo, 300))
+                .retrieve()
+                .bodyToMono(MatrixUploadResponse.class)
+                .block();
+        LOG.info("Got json response: {}", matrixUploadResponse.contentUri);
 
-        final MatrixUploadResponse matrixUploadResponse = MAPPER.readValue(contentUpload, MatrixUploadResponse.class);
-
-        final CloseableHttpResponse responseImage = sendRoomMessage(new MatrixImageMessage(photo.getName(), matrixUploadResponse.contentUri));
-        final int statusImage = responseImage.getStatusLine().getStatusCode();
-        final String contentImage = EntityUtils.toString(responseImage.getEntity());
-        if (statusImage >= 200 && statusImage < 300) {
-            LOG.info("Got json response: {}", contentImage);
-        } else {
-            LOG.error("Error reading json, statusUpload {}: {}", statusImage, contentImage);
-        }
+        final JsonNode responseImage = sendRoomMessage(new MatrixImageMessage(photo.getName(), matrixUploadResponse.contentUri));
+        LOG.info("Got json response: {}", responseImage);
     }
 
     public void setRoomUrl(final String roomUrl) {
